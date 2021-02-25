72aa2033a4b184065bb5bd939b51b4a206d34d05
JonathantgScrivenor <77757119+JonathantgScrivenor@users.noreply.github.com>
2021-02-24 20:26:53 -0600

    server files

--- index.js
+++ index.js
@@ -1,74 +1,29 @@
-console.log('Client started.');
+console.log('WS Server Started!');
 
-//websocket
-const Html5WebSocket = require('html5-websocket');
-const ReconnectionWebSocket = require('reconnecting-websocket');
+//dependencies
+const express = require ('express');
+const WebSocket = require ('ws');
+SocketServer = require('ws').Server;
 
-//initialization
-let ws_host = 'localhost';
-let ws_port = '3000';
-const options = { WebSocket: Html5WebSocket };
-const rws = new ReconnectionWebSocket('ws://' + ws_host + ':' + ws_port + '/ws', undefined, options);
-rws.timeout = 1000;
+//local host connection
+const server = express().listen(3000);
 
-rws.addEventListener('open', () => {
-    console.log('[client] connection established');
-    rws.send('message from client')
-    rws.send(JSON.stringify({
-        method: 'set-background-color',
-        params: {
-            color: 'seagreen'
-        }
-    }))
+//web socket server
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+    console.log('[server] client connected.')
+
+    ws.on('close', () => { console.log ('[server] client disconnected')});
+
+    //add chem functionality here
+    ws.on('message', (message) => {
+
+        console.log('[server] message recieved');
+
+        //send message to all clients
+        wss.clients.forEach(function each(client) {
+            client.send(message);
+        });
+    });
 });
-
-rws.addEventListener('message', (e) => {
-    console.log('[client] message recieved: ' + e.data);
-
-    try {
-        let m = JSON.parse(e.data);
-        handleMessage(m);
-    }
-    catch (err) {
-        console.log('[client] Message is not parsable to JSON.');
-    }
-});
-
-rws.addEventListener('close', () => {
-    console.log('[client] connection lost');
-});
-
-rws.onerror = (err) => {
-    if(err.code == 'EHOSTDOWN') {
-        console.log('[client] Error: Server down.');
-        rws.send('message from client')
-    }
-};
-
-
-//handlers
-
-let handlers = {
-    "set-background-color" : function(m) {
-        console.log('[client] set-background-color handler.');
-        console.log('[client] Color is ' + m.params.color);
-    }
-
-};
-
-function handleMessage(m) {
-    if (m.method == undefined) {
-        return;
-    }
-
-    let method = m.method;
-    if (method) {
-        if(handlers[method]) {
-            let handler = handlers[method];
-            handler(m);
-        } 
-        else {
-            console.log('[client] no handler defintiion: ' + method + '.');
-        }
-    }
-}
