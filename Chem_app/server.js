164fe83ba65770f6b5352a6bf5be6f954475fa09
JonathantgScrivenor <77757119+JonathantgScrivenor@users.noreply.github.com>
2021-04-30 08:52:53 -0500

    Update server.js

--- Chem_app/server.js
+++ Chem_app/server.js
@@ -2,11 +2,37 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const port = 8080
+const mongoose = require ('mongoose')
+const AuthRoute = require ('./Routes/auth')
+const morgan = require ('morgan')
+const bodyParser = require ('body-parser')
+const loginbool = require ("./Controllers/AuthController")
 
-app.set('view engine', 'ejs')
+mongoose.connect('mongodb://localhost:27017/testdb', {useNewUrlParser: true} )
+const db = mongoose.connection
+
+db.on('error', (err) => {
+    console.log(err)
+})
+
+db.once('open',() => {
+    console.log("Database Connected.")
+})
+
+app.set('view engine', 'ejs');
+
+app.use(morgan('dev'))
+app.use (bodyParser.urlencoded({extended: true}))
+app.use (bodyParser.json())
+
+//if (loginbool == false){
+app.get('/', (req, res)=>{
+    res.render('login');
+    })
+//}
 
 app.get('/', (req, res)=>{
-    res.render('Welcome');
+    res.render('welcome');
     });
 app.get('/about', (req, res)=>{
         res.render('about');
@@ -18,9 +44,12 @@ app.get('/Definitions', (req, res)=>{
     res.render('Definitions');
 })
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 
 app.listen(port, ()=> {
-    console.log(`Port found on ${port}`) 
+    console.log(`Port found on ${port}`) ;
 })
+
+app.use ("/api", AuthRoute)
+
 
